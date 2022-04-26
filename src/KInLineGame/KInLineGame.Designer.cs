325f063f2a718779cbdc083c52d0f87306bbdd77
ariel.plasencia <ariel.plasencia@gmail.com>
2022-04-26 10:26:59 -0400

    add k in line visual

---
+++ src/KInLineGame/KInLineGame.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace KInLineGame
+{
+    partial class KInLineGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KInLineGame));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dificultyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.board = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(647, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.home;
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.control;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(131, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.cross_circle;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.dificultyToolStripMenuItem,
+            this.informationToolStripMenuItem});
+            this.optionsToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.lifebuoy;
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.question;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // dificultyToolStripMenuItem
+            // 
+            this.dificultyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.dificultyToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.lock_nossl;
+            this.dificultyToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.dificultyToolStripMenuItem.Name = "dificultyToolStripMenuItem";
+            this.dificultyToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.dificultyToolStripMenuItem.Text = "Dificulty";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.aceptar;
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.Cancelar;
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // informationToolStripMenuItem
+            // 
+            this.informationToolStripMenuItem.Image = global::KInLineGame.Properties.Resources.icon_diagram;
+            this.informationToolStripMenuItem.Name = "informationToolStripMenuItem";
+            this.informationToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.informationToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.informationToolStripMenuItem.Text = "Information";
+            this.informationToolStripMenuItem.Click += new System.EventHandler(this.informationToolStripMenuItem_Click);
+            // 
+            // board
+            // 
+            this.board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.board.Location = new System.Drawing.Point(0, 24);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(647, 370);
+            this.board.TabIndex = 1;
+            this.board.Paint += new System.Windows.Forms.PaintEventHandler(this.board_Paint);
+            this.board.MouseClick += new System.Windows.Forms.MouseEventHandler(this.board_MouseClick);
+            this.board.Resize += new System.EventHandler(this.board_Resize);
+            // 
+            // KInLineGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(647, 394);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "KInLineGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "K in Line";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel board;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dificultyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informationToolStripMenuItem;
+    }
+}
+
---
+++ src/KInLineGame/KInLineGame.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KInLineCore;
+
+namespace KInLineGame
+{
+    public partial class KInLineGame : Form
+    {
+        public KInLineGame()
+        {
+            InitializeComponent();
+        }
+
+        Game game = new Game();
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game = new Game(game.Rows, game.Cols, game.K);
+            board.Invalidate();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Do you want to leave the game?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void board_Paint(object sender, PaintEventArgs e)
+        {
+            var gr = e.Graphics;
+            SizeF cellSize = new SizeF(board.ClientSize.Width / (float)game.Cols,
+                board.ClientSize.Height / (float)game.Rows);
+
+            for (int i = 0; i < game.Rows; i++)
+            {
+                for (int j = 0; j < game.Cols; j++)
+                {
+                    switch (game[i, j])
+                    {
+                        case Cells.Empty:
+                            gr.DrawEllipse(Pens.Black, j * cellSize.Width, board.ClientSize.Height - (i + 1) * cellSize.Height, cellSize.Width,
+                                cellSize.Height);
+                            break;
+                        case Cells.Red:
+                            gr.FillEllipse(Brushes.Red, j * cellSize.Width, board.ClientSize.Height - (i + 1) * cellSize.Height, cellSize.Width,
+                                cellSize.Height);
+                            gr.DrawEllipse(Pens.Black, j * cellSize.Width, board.ClientSize.Height - (i + 1) * cellSize.Height, cellSize.Width,
+                                cellSize.Height);
+                            break;
+                        case Cells.Yellow:
+                            gr.FillEllipse(Brushes.Yellow, j * cellSize.Width, board.ClientSize.Height - (i + 1) * cellSize.Height, cellSize.Width,
+                                cellSize.Height);
+                            gr.DrawEllipse(Pens.Black, j * cellSize.Width, board.ClientSize.Height - (i + 1) * cellSize.Height, cellSize.Width,
+                                cellSize.Height);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void board_Resize(object sender, EventArgs e)
+        {
+            board.Invalidate();
+        }
+
+        private void board_MouseClick(object sender, MouseEventArgs e)
+        {
+            SizeF cellSize = new SizeF(board.ClientSize.Width / (float)game.Cols,
+                board.ClientSize.Height / (float)game.Rows);
+            int col = (int)(e.X / cellSize.Width);
+
+            if (game.CanPlay(col))
+            {
+                game.Play(col);
+                board.Invalidate();
+
+                if (game.IsGameOver)
+                    MessageBox.Show("Game Over", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Invalid Column", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game = new Game(8, 8, 4);
+            board.Invalidate();
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game = new Game(10, 10, 5);
+            board.Invalidate();
+        }
+
+        private void informationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string information = "App : K in Line v2.0 \n";
+            information += "Author : Ariel Plasencia Díaz \n";
+
+            MessageBox.Show(information, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string help = "Posición inicial y objetivo del juego:\n";
+            help += "El K en Línea pertenece a la familia de juegos populares ya que sus sencillas reglas se pueden aprender rápidamente ofreciendo un gran entretenimiento durante un largo tiempo. El objetivo de este juego consiste en colocar cuatro fichas en una fila contínua vertical horizontal o diagonalmente. Se juega sobre un tablero que al empezar está vacío.\n";
+
+            help += "\n¿Cómo colocar las fichas?\n";
+            help += "Ambos jugadores sitúan sus fichas(una por movimiento) en el tablero.La regla para colocarlas consiste en que la estas siempre caen hasta abajo. Es decir una ficha puede ser colocada bien en la parte inferior de una columna o bien sobre otra de alguna otra columna. La siguiente imagen muestra un ejemplo de la posición de una partida en curso donde las cruces verdes señalan las casillas donde el jugador puede colocar una nueva ficha.\n";
+
+            help += "\n¿Cómo finalizar el juego?\n";
+            help += "La partida termina si una de las siguientes condiciones se cumple:\n";
+            help += "1. Uno de los jugadores coloca cuatro o más fichas en una línea contínua vertical, horizontal o diagonalmente. Este jugador gana la partida.\n";
+            help += "2. Todas las casillas del tablero están ocupadas y ningún jugador cumple la condición anterior para ganar. En este caso la partida finaliza en empate.\n";
+
+            MessageBox.Show(help, "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
